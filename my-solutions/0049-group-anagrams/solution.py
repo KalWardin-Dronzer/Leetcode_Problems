efce8cf26f9d228dead4894135034b533a050d18
Vivek Kumar <vivek.kumar240043@gmail.com>
2025-07-23 06:08:44 +0000

    Sync LeetCode submission Runtime - 10 ms (87.62%), Memory - 20.4 MB (100.00%)

---
+++ my-solutions/0049-group-anagrams/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortStr(self, s):
+        s1 = list(s)
+        s1.sort()
+        return "".join(s1)
+
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict1 = {}
+
+        for s in strs:
+            key = self.sortStr(s)
+            if key in dict1:
+                dict1[key].append(s)
+            else:
+                dict1[key] = [s]
+        return list(dict1.values())
+        
+        
+
+
+
