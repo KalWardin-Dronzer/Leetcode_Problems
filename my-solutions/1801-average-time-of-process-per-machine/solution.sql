5ce57583bae47c2650bdcbbf1efa91a4ff104ae6
Vivek Kumar <vivek.kumar240043@gmail.com>
2026-01-20 17:59:36 +0000

    Sync LeetCode submission Runtime - 919 ms (5.03%), Memory - 0.0B (100.00%)

---
+++ my-solutions/1801-average-time-of-process-per-machine/solution.sql
@@ -0,0 +1,3 @@
+select machine_id, round(sum(case when activity_type = 'end' then timestamp
+else -timestamp end)/count(distinct process_id), 3) as processing_time from 
+Activity group by machine_id
