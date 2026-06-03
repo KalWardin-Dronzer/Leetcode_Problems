227b7f20b7c41f617bfbe771ab15bcb15e86a971
Vivek Kumar <vivek.kumar240043@gmail.com>
2026-06-03 19:23:16 +0000

    Sync LeetCode submission Runtime - 357 ms (44.49%), Memory - 0.0B (100.00%)

---
+++ my-solutions/2127-employees-whose-manager-left-the-company/solution.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+select employee_id from employees 
+where manager_id not in (select employee_id from employees)
+and manager_id is not null
+and salary<30000
+order by employee_id
