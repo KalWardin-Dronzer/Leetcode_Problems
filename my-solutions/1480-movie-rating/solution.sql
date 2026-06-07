705bc465fc7c7e7a9e38d81de880cde28c7478d1
Vivek Kumar <vivek.kumar240043@gmail.com>
2026-06-07 17:17:10 +0000

    Sync LeetCode submission Runtime - 1570 ms (12.96%), Memory - 0.0B (100.00%)

---
+++ my-solutions/1480-movie-rating/solution.sql
@@ -0,0 +1,13 @@
+(select u.name as results
+from Users u inner join MovieRating mr on u.user_id = mr.user_id
+group by u.user_id, u.name
+order by count(mr.rating) desc, u.name asc limit 1)
+
+union all
+
+(select m.title as results
+from movies m inner join MovieRating mr on m.movie_id = mr.movie_id
+where mr.created_at < '2020-03-01' and mr.created_at >= '2020-02-01'
+group by mr.movie_id , m.title 
+order by avg(mr.rating) desc, m.title asc limit 1
+)
